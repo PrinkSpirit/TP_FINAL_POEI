0987d822ac2d1967368138ffa7b9970b08166da5
Pierre <pierre.massonie@etu.unilim.fr>
2023-07-05 14:33:19 +0200

    Ajout classe Marchandise

---
+++ Marchandise.h
@@ -0,0 +1,29 @@
+/// Auteur: Pierre MASSONIE
+
+#pragma once
+
+#include <string>
+#include <map>
+
+enum class TypeMarchandise
+{
+
+};
+
+struct Marchandise
+{
+	//static int quantite;	// Quantite de marchandise créée depuis le debut du programme
+	//À ajouter à chaque classe fille
+
+	int id = -1;					// Identifiant unique de la marchandise
+	std::string nom = "Inconue";	// Nom de la marchandise
+	TypeMarchandise type;			// Type de marchandise
+
+	float prix = -1.0f;				// Prix de vente de la marchandise
+	float qualite = -1.0f;			// Qualité de la marchandise
+
+	std::map<TypeMarchandise, int> listeIngredients; // Liste des ingredients necessaires pour fabriquer la marchandise
+
+	Marchandise() {};
+};
+
